1368f40cb98293760b9c3fe06624a1179b2f46db
gallodev <contato.gallodev@gmail.com>
2026-03-20 01:37:31 -0300

    feat: validate-object to validate e-mail in deep layer (domain)

--- client_solid/src/application/usecases/create-client.usecase.ts
+++ client_solid/src/application/usecases/create-client.usecase.ts
@@ -1,8 +1,11 @@
-import { Inject } from '@nestjs/common';
+import { Inject, Logger } from '@nestjs/common';
 import { CLIENT_REPOSITORY } from '@/application/tokens/client.repository.token';
 import type { ClientRepository } from '@/domain/repositories/client.repository';
 import type { CreateClientDTO } from '@/presentation/client/dto/create.client.dto';
 import type { ClientResponseDto } from '@/presentation/client/dto/client-response.dto';
+import { Client } from '@/domain/entities/client.entity';
+import { EmailVO } from '@/domain/value-objects/email.vo';
+import { ClientMapper } from '../mappers/client.mapper';
 
 export class CreateClienteUseCase {
   constructor(
@@ -11,10 +14,17 @@ export class CreateClienteUseCase {
   ) {}
 
   async execute(createClientDto: CreateClientDTO): Promise<ClientResponseDto> {
-    const savedClient = await this.clientRepository.save(createClientDto);
-    return {
-      ...savedClient,
-      fetchDateTime: new Date(Date.now()),
-    };
+    const client = new Client(
+      createClientDto.name,
+      new EmailVO(createClientDto.email),
+    );
+
+    const savedClient = await this.clientRepository.save(client);
+
+    if (savedClient.email.equals(new EmailVO('admin@admin.com'))) {
+      Logger.warn('SOMEONE TRY TO ENTER WITH ADMIN EMAIL!');
+    }
+
+    return ClientMapper.toDTO(client);
   }
 }
--- client_solid/src/presentation/client/client.controller.ts
+++ client_solid/src/presentation/client/client.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Get, Body, Post, Query, Param } from '@nestjs/common';
-import { Client } from '@/domain/entities/client.entity';
+import { Controller, Get, Body, Post, Param } from '@nestjs/common';
 import type { CreateClientDTO } from './dto/create.client.dto';
 import { CreateClienteUseCase } from '@/application/usecases/create-client.usecase';
 import { GetClientUseCase } from '@/application/usecases/get-client.usecase';
+import { ClientResponseDto } from './dto/client-response.dto';
 
 @Controller('/clients')
 export class ClientController {
@@ -12,17 +12,19 @@ export class ClientController {
   ) {}
 
   @Post()
-  save(@Body() createClientDto: CreateClientDTO): Promise<Client> {
+  save(@Body() createClientDto: CreateClientDTO): Promise<ClientResponseDto> {
     return this.createClientUseCase.execute(createClientDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(
+    @Param('id') id: string,
+  ): Promise<ClientResponseDto | ClientResponseDto[]> {
     return this.getClientUseCase.execute(id);
   }
 
   @Get()
-  find() {
+  find(): Promise<ClientResponseDto | ClientResponseDto[]> {
     return this.getClientUseCase.execute();
   }
 }
